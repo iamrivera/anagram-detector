e1a6e1b542739c5e332da0c50009057ad5cdd90c
iamrivera <a3m3rivera@gmail.com>
2020-10-16 14:25:20 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
 # Your code goes here!
+
+# class Anagram
+#     attr_accessor :word
+
+#     def initialize(word)
+#         @word = word
+#         @@match = []
+#     end
+
+#     def match(anagram_array)
+#         anagram_array.each do |array_word|
+#             if @word.split("").sort == array_word.split("").sort
+#                 @@match << array_word
+#             end
+#         end
+#         return @@match
+#     end
+
+# end
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagram_array)
+        anagram_array.select {|array_word| @word.split("").sort == array_word.split("").sort}
+    end
+end
